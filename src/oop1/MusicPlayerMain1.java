c4d3c0c0d855cf9afc023b83cfb5c15e2055c5f2
Shin HyeJi <jssin97@gmail.com>
2024-01-19 18:21:03 +0900

    240119: OOP

---
+++ src/oop1/MusicPlayerMain1.java
@@ -0,0 +1,31 @@
+package oop1;
+
+public class MusicPlayerMain1 {
+    public static void main(String[] args) {
+        int volume = 0;
+        boolean isOn = false;
+
+//        음악 플레이어 켜기
+        isOn = true;
+        System.out.println("음악 플레이어 시작");
+
+//        볼륨 증가
+        volume ++;
+        System.out.println("볼륨: " + volume);
+
+        //        볼륨 증가
+        volume ++;
+        System.out.println("볼륨: " + volume);
+
+        //        볼륨 감소
+        volume --;
+        System.out.println("볼륨: " + volume);
+
+        System.out.println("음악 플레이어 상태 확인");
+        if(isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨: "  +volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+    }
+}
